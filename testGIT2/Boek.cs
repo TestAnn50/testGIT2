ac5ff89abfd0bae33dcf23cc5bfe0bfb2374fea9
Ann Colpaert <a.colpaert@skynet.be>
2020-01-31 12:38:42 +0100

    classe boek

---
+++ testGIT2/Boek.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testGIT2
+{
+    class Boek
+    {
+        private string ISBN;
+        private string strAuteur;
+
+        public string ISBN1 { get => ISBN; set => ISBN = value; }
+        public string StrAuteur { get => strAuteur; set => strAuteur = value; }
+    }
+}
